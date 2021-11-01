14fb5cdf78cf4d7bfcd812ec1f80a3af3a0232ed
Daniel Varga <vargadaniel.80@gmail.com>
2021-11-01 21:35:20 +0100

    s3 bucket connector and test ready

--- tests/common/test_s3.py
+++ tests/common/test_s3.py
@@ -1,11 +1,14 @@
 """
 TestS3BucketConnectorMethods
 """
+from io import BytesIO, StringIO
 import os
 import unittest
 
 import boto3
 from moto import mock_s3
+import pandas as pd
+from xetra.common.custom_exceptions import WrongFormatException
 
 from xetra.common.s3 import S3BucketConnector
 
@@ -95,9 +98,152 @@ class TestS3BucketConnectorMethods(unittest.TestCase):
         # tests after method execution
         self.assertTrue(not list_result)
 
+    def test_read_csv_to_df_ok(self):
+        """
+        tests the read_csv_to_df method for
+        reading 1 .csv file from the mocked s3 bucket
+        """
+        # expected results
+        key_exp = 'test.csv'
+        col1_exp = 'col1'
+        col2_exp = 'col2'
+        val1_exp = 'val1'
+        val2_exp = 'val2'
+        log_exp = f'Reading file {self.s3_endpoint_url}/{self.s3_bucket_name}/{key_exp}'
+        # test init
+        csv_content = f'{col1_exp},{col2_exp}\n{val1_exp},{val2_exp}'
+        self.s3_bucket.put_object(Body=csv_content, Key=key_exp)
+        # method execution
+        with self.assertLogs() as logm:
+            df_result = self.s3_bucket_conn.read_csv_to_df(key_exp)
+            # log test after method execution
+            self.assertIn(log_exp, logm.output[0])
+        # test after method execution
+        self.assertEqual(df_result.shape[0], 1)
+        self.assertEqual(df_result.shape[1], 2)
+        self.assertEqual(val1_exp, df_result[col1_exp][0])
+        self.assertEqual(val2_exp, df_result[col2_exp][0])
+        # cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': key_exp
+                    }
+                ]
+            }
+        )
+
+    def test_write_df_to_s3_empty(self):
+        """
+        Tests the write_df_to_s3 method with
+        an empty DataFrame as input
+        """
+        # expected results
+        return_exp = None
+        log_exp = 'The dataframe is empty! No file will be written!'
+        # test init
+        df_empty = pd.DataFrame
+        key = 'key.csv'
+        file_format = 'csv'
+        # method execution
+        with self.assertLogs() as logm:
+            result = self.s3_bucket_conn.write_df_to_s3(df_empty, key, file_format)
+            # log test after method execution
+            self.assertIn(log_exp, logm.output[0])
+        # test after method execution
+        self.assertEqual(return_exp, result)
+
+    def test_write_df_to_s3_csv(self):
+        """
+        Tests the write_df_to_s3 method
+        if writing csv is successful
+        """
+        # expected results
+        return_exp = True
+        df_exp = pd.DataFrame([['A', 'B'], ['C', 'D']], columns = ['col1', 'col2'])
+        key_exp = 'test.csv'
+        log_exp = f'Writing file to {self.s3_endpoint_url}/{self.s3_bucket_name}/{key_exp}'
+        # test init
+        file_format = 'csv'
+        # method execution
+        with self.assertLogs() as logm:
+            result = self.s3_bucket_conn.write_df_to_s3(df_exp, key_exp, file_format)
+            # log test after method execution
+            self.assertIn(log_exp, logm.output[0])
+        # test after method execution
+        data = self.s3_bucket.Object(key=key_exp).get().get('Body').read().decode('utf-8')
+        out_buffer = StringIO(data)
+        df_result = pd.read_csv(out_buffer)
+        self.assertEqual(return_exp, result)
+        self.assertTrue(df_exp.equals(df_result))
+        # cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': key_exp
+                    }
+                ]
+            }
+        )
+
+    def test_write_df_to_s3_parquet(self):
+        """
+        Tests the write_df_to_s3 method
+        if writing parquet is successful
+        """
+        # expected results
+        return_exp = True
+        df_exp = pd.DataFrame([['A', 'B'], ['C', 'D']], columns = ['col1', 'col2'])
+        key_exp = 'test.parquet'
+        log_exp = f'Writing file to {self.s3_endpoint_url}/{self.s3_bucket_name}/{key_exp}'
+        # test init
+        file_format = 'parquet'
+        # method execution
+        with self.assertLogs() as logm:
+            result = self.s3_bucket_conn.write_df_to_s3(df_exp, key_exp, file_format)
+            # log test after method execution
+            self.assertIn(log_exp, logm.output[0])
+        # test after method execution
+        data = self.s3_bucket.Object(key=key_exp).get().get('Body').read()
+        out_buffer = BytesIO(data)
+        df_result = pd.read_parquet(out_buffer)
+        self.assertEqual(return_exp, result)
+        self.assertTrue(df_exp.equals(df_result))
+        # cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': key_exp
+                    }
+                ]
+            }
+        )
+
+    def test_write_df_to_s3_wrong_format(self):
+        """
+        Test the write_df_to_s3 method
+        if a not supported format is given as argument
+        """
+        # expected results
+        df_exp = pd.DataFrame([['A', 'B'], ['C', 'D']], columns = ['col1', 'col2'])
+        key_exp = 'test.parquet'
+        format_exp = 'wrong_format'
+        log_exp = f'The file format {format_exp} is not supported to be written to S3!'
+        exception_exp = WrongFormatException
+        # method execution
+        with self.assertLogs() as logm:
+            with self.assertRaises(exception_exp):
+                self.s3_bucket_conn.write_df_to_s3(df_exp, key_exp, format_exp)
+            # log test after method execution
+            self.assertIn(log_exp, logm.output[0])
+
 if __name__ == "__main__":
     unittest.main()
     #testIns = TestS3BucketConnectorMethods
     #testIns.setUp()
     #testIns.test_list_files_in_prefix_ok()
     #testIns.tearDown()
+    
--- xetra/common/custom_exceptions.py
+++ xetra/common/custom_exceptions.py
@@ -1,1 +1,9 @@
 """Custom Exceptions"""
+
+class WrongFormatException(Exception):
+    """
+    WrongFormatException class
+
+    Exception that can be raised when the format type
+    gives as parameter is not supported.
+    """
